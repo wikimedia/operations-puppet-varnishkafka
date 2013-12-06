bb4b80c575d8a102a0de5f53533a5cecf8e5fbdc
Andrew Otto <acotto@gmail.com>
2013-12-06 16:45:02 -0500

    VarnishkafkaLogster.py - setting slope to positive for some Ganglia metrics
    Change-Id: I766395f8e25afb3b0af43e69ff2841b3eb75b616

--- files/VarnishkafkaLogster.py
+++ files/VarnishkafkaLogster.py
@@ -8,46 +8,85 @@
 ###
 
 from logster.parsers.JsonLogster import JsonLogster
+from logster.logster_helper import MetricObject
 
 class VarnishkafkaLogster(JsonLogster):
+    # Default ganglia slope is 'both', aka GAUGE.
+    # These keys should be given a slope of
+    # 'positive' aka COUNTER.
+    counter_metrics = [
+        'txbytes',
+        'txmsgs',
+        'rx',
+        'txerrs',
+        'rxerrs',
+        'kafka_drerr',
+        'tx',
+        'scratch_toosmall',
+        'txerr',
+        'trunc',
+        'scratch_tmpbufs',
+    ]
+
+    # metric keys to skip.
+    skip_metrics = [
+        'app_offset',
+        'commited offset',
+        'desired',
+        'eof_offset',
+        'fetch_state',
+        'fetchq_cnt',
+        'fetchq_cnt',
+        'leader',
+        'lp_curr'
+        'name',
+        'next_offset',
+        'nodeid',
+        'partition',
+        'query_offset',
+        'seq',
+        'time',
+        'topic',
+        'toppars',
+        'ts',
+        'unknown',
+    ]
+
+    def get_metric_object(self, metric_name, metric_value):
+        '''
+        Overrides JsonLogster's get_metric_object() method to
+        manually set slope to positive for counter metrics.
+        '''
+
+        metric_object = JsonLogster.get_metric_object(self, metric_name, metric_value)
+
+        metric_slope = ''
+
+        if metric_name.split(self.key_separator)[-1] in VarnishkafkaLogster.counter_metrics:
+            metric_slope = 'positive'
+
+        metric_object.slope = metric_slope
+        return metric_object
+
     def key_filter(self, key):
+        '''
+        Overrides JsonLogster's key_filter method to
+        filter out irrelevant metrics, and to transform
+        the keys of some to make them more readable.
+        '''
 
-        # metric keys to skip.
-        skip = [
-            'app_offset',
-            'commited offset',
-            'desired',
-            'eof_offset',
-            'fetch_state',
-            'fetchq_cnt',
-            'fetchq_cnt',
-            'leader',
-            'lp_curr'
-            'name',
-            'next_offset',
-            'nodeid',
-            'partition',
-            'query_offset',
-            'seq',
-            'time',
-            'topic',
-            'toppars',
-            'ts',
-            'unknown',
-        ]
-
-        if key in skip:
+        if key in VarnishkafkaLogster.skip_metrics:
             return False
         # prepend appropriate rdkafka or varnishkafka to the key,
         # depdending on where the metric has come from.
         elif key == 'varnishkafka':
-            key = 'kafka%svarnishkafka' % self.key_separator
+            key = 'kafka{0}varnishkafka'.format(self.key_separator)
         elif key == 'kafka':
-            key = 'kafka%srdkafka' % self.key_separator
+            key = 'kafka{0}rdkafka'.format(self.key_separator)
         # don't send any bootstrap rdkafka metrics
         elif 'bootstrap' in key:
             return False
-        # any key separators in the key with '-'
+        # replace any key separators in the key with '-'
         elif self.key_separator in key:
             # this won't do anything if key_separator is '-'
             key = key.replace(self.key_separator, '-')
