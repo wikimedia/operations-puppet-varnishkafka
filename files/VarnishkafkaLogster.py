2cadf71a9e9f51d902254a20878c9d655ac50dac
Andrew Otto <acotto@gmail.com>
2013-12-06 14:27:07 -0500

    Filtering out more irrevant varnishkafka metrics
    Change-Id: I778e9fab77af01612e0cd0fcc897a9969730814b

--- files/VarnishkafkaLogster.py
+++ files/VarnishkafkaLogster.py
@@ -12,18 +12,47 @@ from logster.parsers.JsonLogster import JsonLogster
 class VarnishkafkaLogster(JsonLogster):
     def key_filter(self, key):
 
-        if key == 'varnishkafka':
+        # metric keys to skip.
+        skip = [
+            'app_offset',
+            'commited offset',
+            'desired',
+            'eof_offset',
+            'fetch_state',
+            'fetchq_cnt',
+            'fetchq_cnt',
+            'leader',
+            'lp_curr'
+            'name',
+            'next_offset',
+            'nodeid',
+            'partition',
+            'query_offset',
+            'seq',
+            'time',
+            'topic',
+            'toppars',
+            'ts',
+            'unknown',
+        ]
+
+        if key in skip:
+            return False
+        # prepend appropriate rdkafka or varnishkafka to the key,
+        # depdending on where the metric has come from.
+        elif key == 'varnishkafka':
             key = 'kafka%svarnishkafka' % self.key_separator
         elif key == 'kafka':
             key = 'kafka%srdkafka' % self.key_separator
+        # don't send any bootstrap rdkafka metrics
         elif 'bootstrap' in key:
             return False
+        # any key separators in the key with '-'
         elif self.key_separator in key:
             # this won't do anything if key_separator is '-'
             key = key.replace(self.key_separator, '-')
+        # don't send anything that starts with -
         elif key.startswith('-'):
-            return False
-        elif key in ['name', 'topic', 'toppars', 'time', 'fetch_state', 'ts']:
             return False
 
         return key
